151c652e2ee2e4f2211bd72a8bcfb9ad511e23fa
saitsumeli <saitsumeli@gmail.com>
2023-03-06 23:47:34 +0300

    10.Project

---
+++ 10-KeyCodes/script.js
@@ -0,0 +1,21 @@
+const insert = document.querySelector('.insert')
+
+
+window.addEventListener('keydown' ,  (e) => {
+insert.innerHTML = ` <div class="key">
+${e.key  === ' ' ? 'Space' : e.key} 
+<small>event.key</small>
+</div>
+
+<div class="key">
+${e.keyCode}
+<small>event.keyCode</small>
+</div>
+<div class="key">
+${e.code}
+<small>event.code</small>
+</div>
+<div class="key">
+keyCode'u almak için herhangi bir tuşa basın
+</div> `
+})
